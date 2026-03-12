894183cea8daf139e896e0613d09e864d7f3751b
ArvydasG <gailiunas.sapa@gmail.com>
2026-03-12 16:06:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ function toLocalISOString(date) {
     return `${y}-${m}-${d}`;
 }
 
+function getEaster(year) {
+    const f = Math.floor,
+        G = year % 19,
+        C = f(year / 100),
+        H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30,
+        I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11)),
+        J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
+        L = I - J,
+        month = 3 + f((L + 40) / 44),
+        day = L + 28 - 31 * f(month / 4);
+    return new Date(year, month - 1, day);
+}
+
 function getLithuanianHolidays(year) {
     const easter = getEaster(year);
     const easterMonday = new Date(easter);
